dc4c10261e764f8a9d7835f3c5e9b86c0eec874e
Rob <rdaly1490@gmail.com>
2015-05-27 14:38:03 -0500

    styling done and password hidden

--- dist/main.js
+++ dist/main.js
@@ -9,6 +9,23 @@ var $user = $("#username");
 var $pw = $("#password");
 var $signIn = $("#sign-in");
 
+$signIn.on("click", logIn);
+
+function logIn(e) {
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
 $top.css("width", "100%");
 $top.css("height", "350px");
 $top.css("background", "url(bg-login-top.png)");
@@ -25,24 +42,40 @@ $logo.css("marginRight", "auto");
 $logo.css("width", "30%");
 $logo.css("height", "150px");
 
-$user.css("display", "block");
-$user.css("marginBottom", "10px");
-$user.css("marginLeft", "auto");
-$user.css("marginRight", "auto");
-$user.css("width", "40%");
+$user.css("display", "inline-block");
+$user.css("width", "70%");
+$user.css("textAlign", "left");
+$user.css("marginLeft", "15px");
+$user.css("float", "right");
 
-$pw.css("display", "block");
-$pw.css("marginBottom", "10px");
-$pw.css("marginLeft", "auto");
-$pw.css("marginRight", "auto");
-$pw.css("width", "40%");
+$pw.css("display", "inline-block");
+$pw.css("width", "70%");
+$pw.css("textAlign", "left");
+$pw.css("marginLeft", "22px");
+$pw.css("float", "right");
 
-$("span").css("color", "white");
+$signIn.css("float", "right");
+$signIn.css("backgroundColor", "#89bc3f");
+$signIn.css("borderRadius", "5px");
+$signIn.css("width", "25%");
+$signIn.css("height", "30px");
+$signIn.css("color", "white");
+$signIn.css("fontWeight", "bold");
+
+$("#container").css("width", "50%");
+$("#container").css("marginLeft", "auto");
+$("#container").css("marginRight", "auto");
+
+$("#bottom #container div").css("width", "100%");
+$("#bottom div").css("marginLeft", "auto");
+$("#bottom div").css("marginRight", "auto");
+$("#bottom div").css("marginBottom", "20px");
+$("#bottom div").css("color", "white");
 
 // $(".img-responsive").css("backgroundPosition", "center");
 // $(".img-responsive").css("backgroundSize", "cover");
-$(".img-responsive").css("height", "auto");
-$(".img-responsive").css("width", "100%");
+$(".img-responsive").css("height", "60%");
+$(".img-responsive").css("maxWidth", "100%");
 $(".img-responsive").css("display", "block");
 
 
